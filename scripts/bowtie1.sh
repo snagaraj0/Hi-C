9f15f637fa6803e55ffbb955f493df1d8579c371
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-06-02 17:22:19 -0500

    Add files via upload

---
+++ scripts/bowtie1.sh
@@ -0,0 +1,3 @@
+pipenv run python spark.py /s1/snagaraj/project_env/atac01.fq /s1/snagaraj/project_env/atac02.fq  /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome  pair.sam
+
+#$1 $2 $3
