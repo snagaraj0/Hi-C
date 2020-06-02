eb60030239a5963fa8f45ed35615525c3b2236ce
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-06-02 16:56:24 -0500

    Update BowtieSpark.py

--- scripts/BowtieSpark.py
+++ scripts/BowtieSpark.py
@@ -9,25 +9,24 @@ import subprocess
 import sys
 import pydoop.hdfs as hdfs
 from operator import add
-
-exec_mem = sys.argv[4]
-driver_mem = sys.argv[5]
-max_cores = sys.argv[6]
+import logging
 
 start = time.time()
 conf = SparkConf().setAppName("SparkHDFSTEST")
 conf = conf.set('spark.submit.deploymode', "cluster")
-conf = conf.set('spark.executor.memory', exec_mem).set('spark.driver.memory', driver_mem).set("spark.cores.max", max_cores)
+conf = conf.set('spark.executor.memory', '4G').set('spark.driver.memory', '100G').set("spark.cores.max", "165")
+#conf = conf.set("spark.dynamicAllocation.enabled", "true").set("spark.dynamicAllocation.minExecutors","5").set("spark.dynamicAllocation.initialExecutors", "5").set("spark.dynamicAllocation.maxExecutors","5")
 sc = SparkContext.getOrCreate(conf=conf)
+#sc._jsc.hadoopConfiguration().set("fs.defaultFS", "hdfs://greg-hn:9000")
 print(sc.getConf().getAll())
 
 
-
+logging.basicConfig(filename='single.log', filemode='w', format='%(name)s - %(message)s')
 test_input = sys.argv[1]
 
-subprocess.call(["hdfs", "dfs", "-mkdir", "-p", "/user/"])
 subprocess.call(["hdfs", "dfs", "-mkdir", "-p", "/user/data"])
 subprocess.call(["hdfs", "dfs", "-put", test_input, "/user/data" ])
+#subprocess.call(["hdfs", "dfs", "-put", o_input, "/user/data" ])
 
 test_input = test_input.split('/')
 test_len = len(test_input) - 1
@@ -43,11 +42,13 @@ print(input_file)
 # label each line with its positional number
 text_input = (sc.textFile(input_file)).zipWithIndex()
 
-print(text_input.take(10))
+test = text_input.take(10))
+logging.debug(test)
 # map every 4 strings under the same read number
 
 map_text_input = text_input.map(lambda read: (math.floor(read[1]/4), read[0]))
-print(map_text_input.take(20))
+test = map_text_input.take(10)
+logging.debug(test)
 
 # Combine all strings with same read number together
 def create(reads):
@@ -63,18 +64,16 @@ def create(reads):
 
 
 read_map = map_text_input.groupByKey().mapValues(lambda read: create(read))
-print(read_map.take(20))
+test = read_map.take(20))
+logging.debug(test)
+
 
 # Sort by the line number and then grab all the values associated with that line number
 readsRDD = read_map.sortByKey().values()
-num_reads = read_map.count()
-print(num_reads)
-
 bowtie_index = sys.argv[2]
 
 #starts bowtie with parameters to bowtie index.
-alignment_pipe = readsRDD.pipe(st + "/bowtie2 --no-sq --no-hd -x " + bowtie_index + " -")
-
+alignment_pipe = readsRDD.pipe("/s1/snagaraj/bowtie2/bowtie2 --no-sq --no-hd -x " + bowtie_index + " -")
 
 def create_sam(output):
     try:
@@ -87,11 +86,10 @@ def create_sam(output):
 
 
 check=alignment_pipe.getNumPartitions()
-print(check)
-#Write to SAM file by putting together all partitions
+logging.debug("Number of partitions:" + str(check))
+
+# Write partitions to SAM file
 aligned_output = alignment_pipe.foreachPartition(lambda output: create_sam(output))
 end = time.time()
-temp_file = open("time.txt", 'a+')
-temp_file.write("Runtime: " + str(end-start))
-temp_file.close()
+logging.info("Runtime: " + str(end-start))
 sc.stop()
