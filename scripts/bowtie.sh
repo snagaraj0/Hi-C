1c1bbe4fdf4d384f5893e08e84cb95c30d7d7675
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-06-03 12:26:29 -0500

    Update bowtie.sh

--- scripts/bowtie.sh
+++ scripts/bowtie.sh
@@ -1,3 +1,5 @@
-pipenv run python BowtieSpark.py /s1/snagaraj/project_env/SRR639031_1.fastq /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome /s1/snagaraj/project_env/single.sam "--no-hd --no-sq" 4G 100G 100
+pipenv run python SingleSpark.py bowtie2 /s1/snagaraj/project_env/SRR639031_1.fastq /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome /s1/snagaraj/project_env/single.sam "--no-hd --no-sq" 4G 100G 100
+
+pipenv run python SingleSpark.py STAR /s1/snagaraj/project_env/SRR639031_1.fastq /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome /s1/snagaraj/project_env/single.sam "--genome-Dir " 4G 100G 100
 
 #$1 $2 $3
