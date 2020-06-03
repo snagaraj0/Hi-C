90a1d37b9ea5de1065caa33f006f4bcd51cd11b5
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-06-02 21:45:43 -0500

    Add files via upload

---
+++ scripts/valid_reads.sh
@@ -0,0 +1,6 @@
+#pipenv run python duplicates.py single.sam single_r.sam
+
+#pipenv run python duplicates.py single1.sam single_r1.sam
+
+
+pipenv run python duplicates.py benchmark8.sam benchmark_r.sam
