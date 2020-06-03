7ba009d66d3417adb5f012b25e7d4db97b20645f
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-06-02 21:48:05 -0500

    Update valid_reads.sh

--- scripts/valid_reads.sh
+++ scripts/valid_reads.sh
@@ -1,6 +1,1 @@
-#pipenv run python duplicates.py single.sam single_r.sam
-
-#pipenv run python duplicates.py single1.sam single_r1.sam
-
-
-pipenv run python duplicates.py benchmark8.sam benchmark_r.sam
+pipenv run python duplicates.py single.sam single_r.sam
