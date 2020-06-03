4ab890e4dacd545d219e57e458963626d77204d2
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-06-02 21:51:56 -0500

    Update valid_reads.py

--- scripts/valid_reads.py
+++ scripts/valid_reads.py
@@ -2,7 +2,6 @@ import sys
 reads_seen = set() # holds reads already seen
 file = open(sys.argv[1], "r")
 outfile = open(sys.argv[2] , "w")
-debug = open("outr.sam", "w")
 error = open("error.sam", "w")
 temp=0
 
@@ -17,30 +16,7 @@ for line in file:
              continue
     except: 
         continue
-          #error.write(line)
-'''
-for line in file:
-    line_split = line.split()
-    read_id = line_split[0]
-    outfile.write(line)
-    reads_seen.add(read_id)
-    if(read_id[1:] == "PG"):
-       break;
-
-for line in file:
-    line_split = line.split()
-    read_id = line_split[0]
-    keep = True
-    #print(temp)
-    for i in range(11, len(line_split)):
-        if(len(line_split[i].split(":")) != 3):
-           keep = False
-    if read_id not in reads_seen and keep and len(line_split) > 10:
-       if len(line_split[9]) == len(line_split[10]) and len(line_split[2]) > 3:
-           outfile.write(line)
-          #print(line_split[2])
-           reads_seen.add(read_id)
-
-'''
+          error.write(line)
+            
 file.close()
 outfile.close()
