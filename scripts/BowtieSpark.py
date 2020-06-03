720f97872fcda6c34f5d113758ceae53b510dc41
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-06-02 22:50:17 -0500

    Update BowtieSpark.py

--- scripts/BowtieSpark.py
+++ scripts/BowtieSpark.py
@@ -22,7 +22,8 @@ conf = conf.set('spark.executor.memory', exec_mem).set('spark.driver.memory', dr
 sc = SparkContext.getOrCreate(conf=conf)
 print(sc.getConf().getAll())
 
-logging.basicConfig(filename='single.log', filemode='w', format='%(name)s - %(message)s')
+logging.basicConfig(filename='single.log', filemode='w', level=logging.INFO)
+logging.getLogger().setLevel(logging.INFO)
 test_input = sys.argv[1]
 
 subprocess.call(["hdfs", "dfs", "-mkdir", "-p", "/user/data"])
@@ -34,7 +35,12 @@ end_input = test_input[test_len]
 
 st = sys.argv[3]
 filestr= st
-print(filestr)
+logging.info(filestr)
+
+bowtie_exec = filestr.split('/')
+string_exec = ""
+for i in range(len(bowtie_exec) - 1):
+    string_exec += bowtie_exec[i]
 
 input_file = "hdfs:/user/data/" + end_input
 print(input_file)
@@ -43,12 +49,12 @@ print(input_file)
 text_input = (sc.textFile(input_file)).zipWithIndex()
 
 test = text_input.take(10)
-logging.debug(test)
+logging.info(test)
 # map every 4 strings under the same read number
 
 map_text_input = text_input.map(lambda read: (math.floor(read[1]/4), read[0]))
 test = map_text_input.take(10)
-logging.debug(test)
+logging.info(test)
 
 # Combine all strings with same read number together
 def create(reads):
@@ -65,7 +71,7 @@ def create(reads):
 
 read_map = map_text_input.groupByKey().mapValues(lambda read: create(read))
 test = read_map.take(20)
-logging.debug(test)
+logging.info(test)
 
 
 # Sort by the line number and then grab all the values associated with that line number
@@ -73,7 +79,7 @@ readsRDD = read_map.sortByKey().values()
 bowtie_index = sys.argv[2]
 
 #starts bowtie with parameters to bowtie index.
-alignment_pipe = readsRDD.pipe("/s1/snagaraj/bowtie2/bowtie2 --no-sq --no-hd -x " + bowtie_index + " -")
+alignment_pipe = readsRDD.pipe(string_exec + "/bowtie2 " + options + " -x " + bowtie_index + " -")
 
 def create_sam(output):
     try:
@@ -86,7 +92,7 @@ def create_sam(output):
 
 
 check=alignment_pipe.getNumPartitions()
-logging.debug("Number of partitions:" + str(check))
+logging.info("Number of partitions:" + str(check))
 
 # Write partitions to SAM file
 aligned_output = alignment_pipe.foreachPartition(lambda output: create_sam(output))
