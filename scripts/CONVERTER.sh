5847fff26e21762119795e22087307d4a37e7908
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-06-27 12:29:35 -0500

    Update CONVERTER.sh

--- scripts/CONVERTER.sh
+++ scripts/CONVERTER.sh
@@ -3,15 +3,15 @@
 # Script to create single file with all interactions data from 2 single-end SAM files
 
 #  extract important information from sam file
-awk '{print $1,$3,$4,$2,$5;}' $1 > p1.sam.0
-awk '{print $1,$3,$4,$2,$5;}' $2 > p2.sam.0
+awk '{print $1,$3,$4,$2,$5;}' $1 > sam
+awk '{print $1,$3,$4,$2,$5;}' $2 > sam_1
  
 # Sort according to read id
-sort -V -k1 p1.sam.0 > p1.sam.0.sorted
-sort -V -k1 p2.sam.0 > p2.sam.0.sorted
+sort -V -k1 sam > sorted_sam
+sort -V -k1 sam_1 > sorted_sam_1
 
 
-paste p1.sam.0.sorted p2.sam.0.sorted > p1_p2_merged
+paste sorted_sam sorted_sam_1 > sorted
 
 #check appropriate conditions for pairing of interacting reads
-awk '{if($1 eq $6 && $5>= 30 && $10 >= 30 && substr($2,1,3) == "chr" && substr($7,1,3) == "chr" && $3 ~ /^[0-9]+$/ && $8 ~ /^[0-9]+$/) print $2,$3,$4,$7,$8,$9}'  p1_p2_merged  > $3
+awk '{if($1 eq $6 && $5>= 30 && $10 >= 30 && substr($2,1,3) == "chr" && substr($7,1,3) == "chr" && $3 ~ /^[0-9]+$/ && $8 ~ /^[0-9]+$/) print $2,$3,$4,$7,$8,$9}'  sorted  > $3
