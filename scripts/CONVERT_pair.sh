8c9f858fe8d86602eb430598012551040f810703
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-06-27 12:30:53 -0500

    Update CONVERT_pair.sh

--- scripts/CONVERT_pair.sh
+++ scripts/CONVERT_pair.sh
@@ -1,10 +1,10 @@
 #  condense sam file
-awk '{print $1,$3,$4,$2,$5;}' $1 > pr.sam
+awk '{print $1,$3,$4,$2,$5;}' $1 > pair.sam
 
 # Sort according to read id
-sort -V -k1 pr.sam > pr.sam.sorted
+sort -V -k1 pair.sam > pair_sorted.sam
 
-paste -d " "  - - < pr.sam.sorted  > pr.sam.merged
-awk '{if($1 eq $6 && $5>= 30 && $10 >= 30 && substr($2,1,3) == "chr" && substr($7,1,3) == "chr" && $3 ~ /^[0-9]+$/ && $8 ~ /^[0-9]+$/) print $2,$3,$4,$7,$8,$9}'  pr.sam.merged > $2
+paste -d " "  - - < pair_sorted.sam > pair_sorted_merge.sam
+awk '{if($1 eq $6 && $5>= 30 && $10 >= 30 && substr($2,1,3) == "chr" && substr($7,1,3) == "chr" && $3 ~ /^[0-9]+$/ && $8 ~ /^[0-9]+$/) print $2,$3,$4,$7,$8,$9}' pair_sorted_merge.sam > $2
 
 
