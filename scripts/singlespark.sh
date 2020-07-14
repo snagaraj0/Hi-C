9ff7fce79c13b67a21056d8b31e765033ac958af
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-07-14 14:11:16 -0500

    Update singlespark.sh

--- scripts/singlespark.sh
+++ scripts/singlespark.sh
@@ -1,7 +1,7 @@
 #Example using Bowtie2
-pipenv run python SingleSpark.py bowtie2 /s1/snagaraj/project_env/SRR639031_1.fastq /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome /s1/snagaraj/project_env/single.sam "--no-hd --no-sq" 4G 100G 100
+pipenv run python SingleSpark.py bowtie2 /s1/snagaraj/project_env/SRR639031_1.fastq /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome /s1/snagaraj/project_env/single.sam "--no-hd --no-sq" 20G 100G 100 25G
 
 #Example using STAR
-pipenv run python SingleSpark.py STAR /s1/snagaraj/project_env/SRR639031_1.fastq /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome /s1/snagaraj/project_env/single.sam "--genome-Dir " 4G 100G 100
+pipenv run python SingleSpark.py STAR /s1/snagaraj/project_env/SRR639031_1.fastq /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome /s1/snagaraj/project_env/single.sam "--genome-Dir " 4G 100G 100 25G
 
 #$1 $2 $3
