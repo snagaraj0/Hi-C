c5a12485a8e55b4ae21ddf019e852171eb8ccd3c
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-07-14 14:11:49 -0500

    Update pairspark.sh

--- scripts/pairspark.sh
+++ scripts/pairspark.sh
@@ -1,3 +1,3 @@
-pipenv run python spark.py /s1/snagaraj/project_env/SRR639031_1.fastq /s1/snagaraj/project_env/SRR639031_2.fastq /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome /s1/snagaraj/project_env/single.sam "--no-hd --no-sq" 4G 100G 100
+pipenv run python spark.py /s1/snagaraj/project_env/SRR639031_1.fastq /s1/snagaraj/project_env/SRR639031_2.fastq /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome /s1/snagaraj/project_env/single.sam "--no-hd --no-sq" 20G 100G 100 25G
 
 #$1 $2 $3
