89c434efb087e8d31c092447d4528c31d6f746f9
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-07-14 14:08:06 -0500

    Update singlespark.py

--- scripts/singlespark.py
+++ scripts/singlespark.py
@@ -10,94 +10,80 @@ import sys
 import pydoop.hdfs as hdfs
 import logging
 
+mapper = sys.argv[1]
+fq_input = sys.argv[2]
+bowtie_index = sys.argv[3]
+st = sys.argv[4]
 options = sys.argv[5]
 exec_mem = sys.argv[6]
 driver_mem = sys.argv[7]
 max_cores = sys.argv[8]
+driver_resultSize = sys.argv[9]
 
 start = time.time()
-conf = SparkConf().setAppName("SparkHDFSTEST")
+conf = SparkConf().setAppName("SingleSparkAligner")
 conf = conf.set('spark.submit.deploymode', "cluster")
-conf = conf.set('spark.executor.memory', exec_mem).set('spark.driver.memory', driver_mem).set("spark.cores.max", max_cores)
+conf = conf.set('spark.executor.memory', exec_mem).set('spark.driver.memory', driver_mem).set("spark.cores.max", max_cores).set('spark.driver.maxResultSize', driver_resultSize)
 sc = SparkContext.getOrCreate(conf=conf)
 
-logging.basicConfig(filename='single.log', filemode='w', level=logging.INFO)
+logging.basicConfig(filename='singlespark.log', filemode='w', level=logging.INFO)
 logging.getLogger().setLevel(logging.INFO)
 logging.info(sc.getConf().getAll())
 
-mapper = sys.argv[1]
-test_input = sys.argv[2]
-
 subprocess.call(["hdfs", "dfs", "-mkdir", "-p", "/user/data"])
-subprocess.call(["hdfs", "dfs", "-put", test_input, "/user/data" ])
+subprocess.call(["hdfs", "dfs", "-put", fq_input, "/user/data" ])
 
-test_input = test_input.split('/')
-test_len = len(test_input) - 1
-end_input = test_input[test_len]
-
-st = sys.argv[4]
-filestr= st
+filestr = st
 logging.info(filestr)
 
 bowtie_exec_index = filestr.rfind("/")
 bowtie_exec = ""
 for i in range(bowtie_exec_index + 1):
     bowtie_exec += filestr[i]
+    
+fq_input = fq_input.split('/')
+test_len = len(fq_input) - 1
+end_input = fq_input[test_len]
+
+open(filestr, 'w').close()
 
 input_file = "hdfs:/user/data/" + end_input
 print(input_file)
 
-# label each line with its positional number
-text_input = (sc.textFile(input_file)).zipWithIndex()
+# create key-value pair with (read on line, line number)
+zipped_input = (sc.textFile(input_file)).zipWithIndex()
 
-test = text_input.take(10)
-logging.info(test)
-# map every 4 strings under the same read number
+add = zipped_input.keyBy(lambda x: math.floor(x[1]/4))
+logging.info("Zipped Output", add.takeOrdered(4))
 
-map_text_input = text_input.map(lambda read: (math.floor(read[1]/4), read[0]))
-test = map_text_input.take(10)
-logging.info(test)
+# Combine all strings with the same key together
+def joining_func(line):
+    key = line[0]
+    value_tup = line[1]
+    sort_tup = sorted(value_tup, key = lambda x: x[1])
+    return '\n'.join([y[0] for y in sort_tup])
 
-# Combine all strings with same read number together
-def create(reads):
-    temp = 0
-    line_str = ""
-    for read in reads:
-        if(temp % 4 == 0 and temp/4 == 1):
-            break
-        else:
-            line_str += read + '\n'
-            temp = temp + 1
-    return line_str
-
-
-read_map = map_text_input.groupByKey().mapValues(lambda read: create(read))
-test = read_map.take(20)
-logging.info(test)
-
-
-# Sort by the line number and then grab all the values associated with that line number
-readsRDD = read_map.sortByKey().values()
-bowtie_index = sys.argv[3]
+#Group all lines with the same Key and join them together by their original position in the file
+rdd_add = add.groupByKey().map(joining_func)
+logging.info("Grouped and Joined Output", rdd_add.takeOrdered(4))
 
 #starts bowtie with parameters to bowtie index.
-alignment_pipe = readsRDD.pipe(bowtie_exec + mapper + " " + options + " -x " + bowtie_index + " -")
+alignment_pipe = rdd_add.pipe(bowtie_exec + mapper + " " + options + " -x " + bowtie_index + " -")
+logging.info("Mapper Output", alignment_pipe.take(4))
 
-def create_sam(output):
-    try:
-       file = open(filestr, "a+")
-       for alignment in output:
-           file.write(alignment + '\n')
-       file.close()
-    except Exception as ex:
-       print(ex)
+logging.info("Number of Partitions:", alignment_pipe.getNumPartitions())
+logging.info("Number of Reads:", alignment_pipe.count())
+
+#Collecting output to a single executor to write to local file
+alignment_final = alignment_pipe.collect()
+file = open(filestr, "a+")
+for line in alignment_final:
+    file.write(line + '\n')
+file.close()
 
 
-check=alignment_pipe.getNumPartitions()
-logging.info("Number of partitions:" + str(check))
-
-# Write partitions to SAM file
-aligned_output = alignment_pipe.foreachPartition(lambda output: create_sam(output))
 end = time.time()
-logging.info("Runtime: " + str(end-start))
+temp_file = open("time.txt", 'a+')
+temp_file.write("Runtime: " + str(end-start))
+temp_file.close()
 sc.stop()
