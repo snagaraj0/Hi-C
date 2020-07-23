08758cf65cdbbf6cdf5a347cf229ac5655563cb2
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-07-23 17:39:45 -0500

    Update singlespark.sh

--- scripts/singlespark.sh
+++ scripts/singlespark.sh
@@ -1,5 +1,5 @@
 #Example using Bowtie2
-pipenv run python SingleSpark.py single 20G 100G 100 25G "/s1/snagaraj/bowtie2/bowtie2 --no-hd --no-sq -x /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome -" 
+pipenv run python SingleSpark.py single 20G 100G 100 "/s1/snagaraj/bowtie2/bowtie2 --no-hd --no-sq -x /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome -" 
 
 #Example using STAR
 pipenv run python SingleSpark.py STAR /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome /s1/snagaraj/project_env/single.sam "--genome-Dir " 4G 100G 100 25G
