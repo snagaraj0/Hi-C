710e7476de5ae3d59a5d3974aafaa2b850913161
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-07-23 18:08:00 -0500

    Update pairspark.py

--- scripts/pairspark.py
+++ scripts/pairspark.py
@@ -10,23 +10,23 @@ import sys
 import pydoop.hdfs as hdfs
 import logging
 
-options = sys.argv[5]
-exec_mem = sys.argv[6]
-driver_mem = sys.argv[7]
-max_cores = sys.argv[8]
+test_input = sys.argv[1]
+o_input = sys.argv[2]
+direc_path = sys.argv[3]
+exec_mem = sys.argv[4]
+driver_mem = sys.argv[5]
+max_cores = sys.argv[6]
+options = sys.argv[7]
 
 logging.basicConfig(filename='pair.log', filemode='w', level=logging.INFO)
 
-start = time.time()
+#Uncomment to document time
+#start = time.time()
 conf = SparkConf().setAppName("SparkHDFSTEST")
 conf = conf.set('spark.submit.deploymode', "cluster")
 conf = conf.set('spark.executor.memory', exec_mem).set('spark.driver.memory', driver_mem).set("spark.cores.max", max_cores)
 sc = SparkContext.getOrCreate(conf=conf)
 logging.info(sc.getConf().getAll())
-
-
-test_input = sys.argv[1]
-o_input = sys.argv[2]
 
 subprocess.call(["hdfs", "dfs", "-mkdir", "-p", "/user"])
 subprocess.call(["hdfs", "dfs", "-mkdir", "-p", "/user/data"])
@@ -39,15 +39,7 @@ end_input = test_input[len(test_input) - 1]
 o_input = o_input.split('/')
 end_o_input = o_input[len(o_input)- 1]
 
-st = sys.argv[4]
-filestr= st
-logging.info(filestr)
-
-bowtie_exec_index = filestr.rfind("/")
-bowtie_exec = ""
-for i in range(bowtie_exec_index + 1):
-    bowtie_exec += filestr[i]
-
+logging.info(direc_path)
 
 input_file = "hdfs:/user/data/" + end_input
 print(input_file)
@@ -67,22 +59,6 @@ map_text_input1 = text_input1.map(lambda read: (math.floor(read[1]/4), read[0]))
 test = map_text_input.take(20)
 logging.info(test)
 
-# Combine all strings with same read number together
-def create(reads):
-    temp = 0
-    line_str = ""
-    for read in reads:
-        if(temp % 4 == 0 and temp/4 == 1):
-            break
-        else:
-            line_str += read + '\n'
-            temp = temp + 1
-    return line_str
-    
-
-reads = map_text_input.groupByKey().mapValues(lambda read: create(read))
-reads1 = map_text_input1.groupByKey().mapValues(lambda read: create(read))
-test = reads.take(20)
 logging.info(test)
 
 combineRDD = reads.join(reads1)
@@ -94,31 +70,16 @@ val_combineRDD = combineRDD.sortByKey().values()
 test = val_combineRDD.first()
 logging.info(test)
 
-bowtie_index = sys.argv[3]
-combinedRDD = val_combineRDD.map(lambda x: x[0] + x[1])
-test= combinedRDD.take(20)
-logging.info(test[0])
-
 #starts bowtie with parameters to bowtie index.
 alignment_pipe = combinedRDD.pipe(bowtie_exec + "bowtie2 " + options + " -x " + bowtie_index + " --interleaved " + " -") 
 test = alignment_pipe.take(20)
 logging.info(test)
 
-
-def create_sam(output):
-    try:
-       file = open(filestr, "a+")
-       for alignment in output:
-           file.write(alignment + '\n')
-       file.close()
-    except Exception as ex:
-       print(ex)
-
 check=alignment_pipe.getNumPartitions()
 logging.info("Number of partitions:" + str(check))
 
 # Write partitions to SAM file
-aligned_output = alignment_pipe.foreachPartition(lambda output: create_sam(output))
+
 end = time.time()
 logging.info("Runtime: " + str(end-start))
 sc.stop()
