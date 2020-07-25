d1fac820d4661c07e9dd65239265daed7d100cd1
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-07-24 22:22:46 -0500

    Update singlespark.sh

--- scripts/singlespark.sh
+++ scripts/singlespark.sh
@@ -2,6 +2,6 @@
 python SingleSpark.py /s1/snagaraj/project_env/SRR639031_1.fastq file:///s1/snagaraj/single 20G 100G 100 "/s1/snagaraj/bowtie2/bowtie2 --no-hd --no-sq -x /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome -" 
 
 #Example using STAR
-python SingleSpark.py /s1/snagaraj/project_env/SRR639031_1.fastq file:///s1/snagaraj/single 20G 100G 100  "/s1/snagaraj/STAR/STAR --runMode alignReads --readFilesIn /dev/stdin --readFilesType SAM --genome-Dir /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome --outStd SAM"
+python SingleSpark.py /s1/snagaraj/project_env/SRR639031_1.fastq file:///s1/snagaraj/single 20G 100G 100  "/s1/snagaraj/STAR/STAR --runMode alignReads --readFilesIn /dev/stdin --readFilesType Fastx SE --genome-Dir /s1/snagaraj/star --outStd SAM"
 
 #$1 $2 $3
