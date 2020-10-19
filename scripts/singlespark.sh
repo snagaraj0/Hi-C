76beeb75a60631dd0bcd5221b6d3ef83d4ab2310
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-10-19 17:45:27 -0500

    Update singlespark.sh

--- scripts/singlespark.sh
+++ scripts/singlespark.sh
@@ -1,10 +1,10 @@
 #Example using Bowtie2
-python singlespark.py /s1/snagaraj/project_env/SRR639031_1.fastq file:///s1/snagaraj/single 20G 100G 100 2 "/s1/snagaraj/bowtie2/bowtie2 --no-hd --no-sq -x /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome -" 
+python singlespark.py /s1/snagaraj/project_env/SRR639031_1.fastq /s1/snagaraj/output/single 20G 100G 100 2 "/s1/snagaraj/bowtie2/bowtie2 --no-hd --no-sq -x /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome -" 
 
 #Example using HISAT2
-python singlespark.py /s1/snagaraj/project_env/SRR639031_1.fastq file:///s1/snagaraj/single 20G 100G 100 2 "/s1/snagaraj/hisat2/hisat2 --no-hd --no-sq -p 50 -x /s1/snagaraj/grch38/genome -" 
+python singlespark.py /s1/snagaraj/project_env/SRR639031_1.fastq /s1/snagaraj/output/single 20G 100G 100 2 "/s1/snagaraj/hisat2/hisat2 --no-hd --no-sq -p 50 -x /s1/snagaraj/grch38/genome -" 
 
 #Example using BBMAP
-python singlespark.py /s1/snagaraj/project_env/SRR639031_1.fastq file:///s1/snagaraj/single 20G 100G 100 2 "java -ea -Xmx40912m -Xms30312m -cp /s1/snagaraj/bbmap/current/align2.BBMap build=1 overwrite=true fastareadlen=500 in=stdin.fq out=stdout ref=/s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome.fa interleaved=false path=/s1/snagaraj/bbmap build=1 t=50"
+python singlespark.py /s1/snagaraj/project_env/SRR639031_1.fastq /s1/snagaraj/output/single 20G 100G 100 2 "java -ea -Xmx40912m -Xms30312m -cp /s1/snagaraj/bbmap/current/align2.BBMap build=1 overwrite=true fastareadlen=500 in=stdin.fq out=stdout ref=/s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome.fa interleaved=false path=/s1/snagaraj/bbmap build=1 t=50"
 
 #$1 $2 $3
