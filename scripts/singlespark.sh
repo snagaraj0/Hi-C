427d1289ce102e98fbfb8311ddadf9bb0575708d
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-10-20 18:57:56 -0500

    Update singlespark.sh

--- scripts/singlespark.sh
+++ scripts/singlespark.sh
@@ -1,5 +1,5 @@
 #Example using Bowtie2
-python singlespark.py /s1/snagaraj/project_env/SRR639031_1.fastq file:///s1/snagaraj/output/single 20G 100G 100 2 "/s1/snagaraj/bowtie2/bowtie2 --no-hd --no-sq -x /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome -" 
+python singlespark.py /s1/snagaraj/project_env/SRR639031_1.fastq file:///s1/snagaraj/output/single 20G 100G 100 2 "/s1/snagaraj/bowtie2/bowtie2 --no-hd --no-sq -p 50 -x /s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome -" 
 
 #Example using HISAT2
 python singlespark.py /s1/snagaraj/project_env/SRR639031_1.fastq file:///s1/snagaraj/output/single 20G 100G 100 2 "/s1/snagaraj/hisat2/hisat2 --no-hd --no-sq -p 50 -x /s1/snagaraj/grch38/genome -" 
